5d6aa99d688201d761895cfc7ff3e3cc071fc444
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 21:37:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,4 +13,19 @@ module "sub_compartments" {
   count = length(var.subcompartments) > 0 ? 1 : 0
 }
 
+  module "base_vcn" {
+  source              = "./vcn"
+  network_compartment = var.network_compartment
+  vcn_dns_label       = var.vcn_dns_label
+}
+
+module "base_subnet" {
+  source              = "./subnets"
+  subnets             = var.subnets
+  vcn_id              = module.base_vcn.vcn_id_output
+  network_compartment = var.network_compartment
+
+  count = length(var.subnets) > 0 ? 1 : 0
+}
+
 
