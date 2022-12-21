1e952d3803466f2d0237305f754d6fb34aaa91c6
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 17:57:18 +0530

    Create compartment.tf

---
+++ compartments/main_compartment/compartment.tf
@@ -0,0 +1,6 @@
+resource "oci_identity_compartment" "Retail_compartment" {
+    compartment_id = "${var.compartment_id}"
+    name = var.compartment_name
+    description = var.compartment_description
+    
+}
