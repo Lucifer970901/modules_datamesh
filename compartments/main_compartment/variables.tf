5294a25c508a38316f8dfd61a9b39947f94766fe
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 17:58:15 +0530

    Create variables.tf

---
+++ compartments/main_compartment/variables.tf
@@ -0,0 +1,16 @@
+variable "compartment_description" {
+  default      = "This is the main compartment for all the retail resources"
+  type        = string
+}
+
+variable "compartment_id" {
+  description = "parent compartment id under which this compartment is created"
+  type        = string
+
+}
+
+variable "compartment_name" {
+  description = "user-friendly name of to use for the compartment"
+  type        = string
+  default     = "Retail"
+}
