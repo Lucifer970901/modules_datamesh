8ffcefa93c21f65358f197104f1a6d7efd1ea066
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 20:17:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "sub_compartments" {
   source              = "./compartments/sub_compartments"
   sub_compartments    = var.sub_compartments
   compartment_id      = module.main_compartment.compartment_id_output
-  network_compartment = var.network_compartment
+  
 
   count = length(var.sub_compartments) > 0 ? 1 : 0
 }
