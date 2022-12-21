3f49624996dcc58eb983462c2dac97bb32a3a46e
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 21:40:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ module "sub_compartments" {
 }
 
   module "base_vcn" {
-  source              = "./vcn"
+  source              = ".Networking/vcn"
   network_compartment = var.network_compartment
   vcn_dns_label       = var.vcn_dns_label
 }
 
 module "base_subnet" {
-  source              = "./subnets"
+  source              = ".Networking/subnets"
   subnets             = var.subnets
   vcn_id              = module.base_vcn.vcn_id_output
   network_compartment = var.network_compartment
