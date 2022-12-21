36e992e1f7905fade7d9a5a48ff1c4c15bf4acc5
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 18:26:23 +0530

    Update compartment.tf

--- compartments/main_compartment/compartment.tf
+++ compartments/main_compartment/compartment.tf
@@ -1,5 +1,5 @@
 resource "oci_identity_compartment" "Retail_compartment" {
-    compartment_id = "${var.parent_compartment_id}"
+    compartment_id = "${var.parent_compartment}"
     name = var.compartment_name
     description = var.compartment_description
     enable_delete = "true"
