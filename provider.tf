f3a2b78ec7dcd84f1072679a6533d0643792429d
Meghashree N <meghashreetendulkar@gmail.com>
2022-12-21 17:54:37 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "oci" {
+  version          = ">=3.11"
+  tenancy_ocid     = var.tenancy_id
+  user_ocid        = var.user_id
+  fingerprint      = var.api_fingerprint
+  private_key_path = var.api_private_key_path
+  region           = var.region
+}
